748b074ab9823f5794754b52130e2bd6626d8757
Mina Jung <octavesop@gmail.com>
2021-06-13 02:34:42 +0900

    to do list 기본 틀 완성.
    다음엔 index를 붙여 동작 확인 및 필터 기능 부착.

---
+++ todortk/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <h1>To-Do-List</h1>
+    <input type="text" @change=handleFileUpload>
+    <input type="submit">
+  </v-container>
+</template>
+
+<script>
+//import AWS from 'aws-sdk'
+
+export default {
+  /*
+  data: {
+    
+  },
+  */
+  methods: {
+    handleFileUpload() {
+      console.log("업로드 댓당 야호~")
+    }
+  }
+}
+</script>
